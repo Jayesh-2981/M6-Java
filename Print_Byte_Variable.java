dbeeb9f81808a0ab83ed25a508b59ed1f9529317
Jayesh Jadhav <114849888+Jayesh-2981@users.noreply.github.com>
2024-02-07 20:06:20 +0530

    Uploading Examples of Byte and Short Variables

---
+++ Print_Byte_Variable.java
@@ -0,0 +1,22 @@
+// This is a example of byte variable
+
+class Print_Byte_Variable
+{
+	//This is main method
+	
+public static void main(String[]pbv)
+{
+	//This is a Declaration+Initialization of variable 
+	
+short variable1=20;
+
+//This is printing the variable
+
+System.out.println(variable1);
+
+}
+}
+
+/*output:
+	Compile Time: Success
+	RunTime:Success:20*/
---
+++ Print_Short_Variable.java
@@ -0,0 +1,22 @@
+// This is a example of short variable
+
+class Print_Short_Variable
+{
+	//This is main method
+	
+public static void main(String[]psv)
+{
+	//This is a Declaration+Initialization of variable 
+	
+short variable2=1000;
+
+//This is printing the variable
+
+System.out.println(variable2);
+
+}
+}
+
+/*output:
+	Compile Time: Success
+	RunTime:Success:1000*/
