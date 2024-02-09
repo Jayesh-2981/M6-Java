ea2cdae62a2a8996742c31ca3c82ec3c075911e8
Jayesh Jadhav <114849888+Jayesh-2981@users.noreply.github.com>
2024-02-09 19:39:53 +0530

    Uploading example of float variable

---
+++ Print_Float_Variable.java
@@ -0,0 +1,14 @@
+//This is the example of float variable
+
+class Print_Float_Variable
+{
+	public static void main(String [] pfv)
+	{
+		float variable=50.856954f;
+		System.out.println(variable);
+	}
+}
+
+/* Output:
+	CT: success
+	RT: success:50.856953*/
