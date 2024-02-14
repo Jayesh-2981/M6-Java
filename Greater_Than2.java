1f66ac4c3c0d33e7050e0f705e42fd8d80d06768
Jayesh Jadhav <114849888+Jayesh-2981@users.noreply.github.com>
2024-02-14 21:24:25 +0530

    Uploading Realational operators examples

---
+++ Greater_Than2.java
@@ -0,0 +1,17 @@
+class Greater_Than2
+{
+	public static void main(String [] gt)
+	{
+		int value1=25;
+		int value2=26;
+		
+		boolean result = value1>value2;
+		
+		System.out.println("L.H.S. is greater than R.H.S. "+result);
+	}
+}
+
+/*
+CT: success;
+RT: success: L.H.S. is greater than R.H.S. false
+*/
