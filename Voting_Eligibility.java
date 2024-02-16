e3ac7eb42614ebadf25fce837a52f0fb6d102e42
Jayesh Jadhav <114849888+Jayesh-2981@users.noreply.github.com>
2024-02-16 20:27:52 +0530

    Adding nested ternary operators examples

---
+++ Voting_Eligibility.java
@@ -0,0 +1,14 @@
+class Voting_Eligibility
+{
+	public static void main(String [] ve)
+	{
+	int age=23;
+	String result=(age>18)?("Eligible for voting"):("Not eligible for voting");
+	System.out.println(result);
+	}
+}
+
+/* Output
+CT: success;
+RT: success: Eligible for voting
+*/
