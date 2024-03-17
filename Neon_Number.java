9bc58d171b321dec78aff93e3ad09efaa37efe01
Jayesh Jadhav <114849888+Jayesh-2981@users.noreply.github.com>
2024-03-17 20:57:49 +0530

    Adding some numbers program

---
+++ Neon_Number.java
@@ -0,0 +1,31 @@
+class Neon_Number
+{
+	public static void main(String [] nn)
+	{
+		int num=9;
+		int sum=0;
+		int square=num*num;
+		
+		while(square>0)
+		{
+			int digit=square%10;
+			sum+=digit;
+			square/=10;
+		}
+		
+		if(sum==num)
+		{
+			System.out.println("Number is neon number");
+		}			
+		else
+		{
+			System.out.println("Number is not a neon number");
+		}
+}
+}
+
+/* 
+OUTPUT
+CT: success;
+RT: success: Number is neon number
+*/
---
+++ Tech_Number.java
@@ -0,0 +1,57 @@
+class Tech_Number
+{
+	public static void main(String [] tn)
+	{
+		int num=1256;
+		
+		// number will be tech number when num=2025,3025 etc.
+		
+		int copy_num=num;
+		int count=0;
+		
+		// Generating count of digit
+		
+		while(num>0)
+		{
+			count++;
+			num/=10;
+		}
+		
+		if(count%2==0)
+		{
+			// Generating Divisor
+			
+			int div=1;
+			
+			for(int i=1; i<=count/2; i++)
+			{
+				div*=10;
+			}
+			
+			int first_Half = copy_num/div;
+			int second_Half = copy_num%div;
+			
+			int sum=first_Half+second_Half;
+			int sumSquare=sum*sum;
+			
+			if(sumSquare==copy_num)
+			{
+				System.out.println("Number is Tech number");
+			}
+			else
+			{
+				System.out.println("Number is not Tech Number");
+			}
+		}
+		else
+		{
+			System.out.println("Number is no even digit");
+		}
+	}
+}
+
+/* 
+OUTPUT
+CT: success;
+RT: success: Number is not Tech Number
+*/
