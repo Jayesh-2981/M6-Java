48c4b90a40bf40d0a27354d178e7f4b960557544
Jayesh Jadhav <114849888+Jayesh-2981@users.noreply.github.com>
2024-03-21 23:40:40 +0530

    Adding some numbers programs

---
+++ Sunny_Number.java
@@ -0,0 +1,30 @@
+class Sunny_Number
+{
+	public static void main(String [] sn)
+	{
+		int num=15;
+		int result=1;
+		
+		for(int i=1; result<=num+1; i++)
+		{
+			result=i*i;
+			
+			if(result==num+1)
+			{
+				System.out.println("Given number is a Sunny Number");
+				break;
+			}
+		}
+		
+		if(result>num+1)
+		{
+			System.out.println("Given number is not a Sunny Number");
+		}
+	}
+}
+
+/* 
+OUTPUT
+CT: success;
+RT: success: Given number is a Sunny Number
+*/
